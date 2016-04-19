6f8a6aae6b2e92d587dffd38a55686c725fd60c5
Brandon Qiao <qiaobrandon@gmail.com>
2016-04-18 21:31:30 -0400

    So i can get the file on the Pi

---
+++ main.py
@@ -0,0 +1,36 @@
+import ystockquote
+import time
+from decimal import Decimal
+stock_list= ['%5EIXIC', '%5EGSPC', 'INCY']
+
+    
+def get_time():
+    return time.strftime("%I:%M")
+def get_percent_change(symbol):
+    change = ystockquote.get_change(symbol)
+    price = ystockquote.get_price(symbol)
+    percent_change = 100*float(change)/(float(price)-float(change))
+    percent_decimal = round(Decimal(percent_change), 2)
+    if percent_decimal > 0:
+        return '+'+str(percent_decimal)
+    elif percent_decimal==0:
+        return '0'
+    else:
+        return '-'+str(abs(percent_decimal))
+def stocks():
+    stock_output = ''
+    for stock in stock_list:
+        #print(stock)
+        #print(get_percent_change(stock))
+        stock_output = stock_output+stock+' '+get_percent_change(stock)+' '
+        #stock_output = stock_output+' '+get_percent_change(stock)
+    return stock_output
+
+
+        
+
+
+#print(get_percent_change('GOOG'))
+#print(get_percent_change('INCY'))
+#print isinstance(get_percent_change('INCY'), str)
+print(stocks())
